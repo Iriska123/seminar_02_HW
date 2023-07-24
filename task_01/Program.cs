0aea384774fab5a69cd21afc03772fc885450818
Irina <irinapetr1110@mail.ru>
2023-07-24 22:30:15 +0300

    add tast 01

--- task_01/Program.cs
+++ task_01/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное  и на выходе показывает вторую цифру этого числа. (Если решаете математически через рандом, то проверка не нужна на трехзначность. Если через строки, то она обязательна)
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+
+Console.WriteLine("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($" Вы ввели число - {num}");
+
+
+if(num / 100 >= 1 && num / 100 < 10) {
+  Console.WriteLine("Число трехзначное ");
+  int x = num/10;
+  int x2 = x%10;
+  Console.WriteLine($"вторая цифра - {x2} ");
+
+} else{
+  Console.WriteLine("Число не трехзначное ");
+}
